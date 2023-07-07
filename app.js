95e0d513cd9709aa28d076b975ab9098d8f88b39
Genexis <juniorkingboy5gmail.com>
2023-07-07 13:02:56 -0500

    finished with the slider Version, now I'll merge it into the sofa branch

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import {
   slides,
+  leftDoor,
+  rightDoor,
+  leftWindow,
+  rightWindow,
+  leftTool,
+  rightTool,
   drillsWrapper,
   drillsImgs,
+  doorsImgs,
+  windowImgs,
   drillsTag,
   hammersTag,
   screwDriverTag,
   miscllnTag,
+  doorsImgsArray,
+  windowImgsArray,
+  defaultToolsImgsPathArray,
   drillsImgsPathArray,
   hammersImgsPathArray,
   screwDrvImgsPathArray,
@@ -20,6 +31,7 @@ drillsTag.addEventListener("click", (e)=>{
   {
    drillsImgs[i].setAttribute('src', drillsImgsPathArray[i]);
    drillsImgs[i].setAttribute('alt', "une perceuse");
+   defaultToolsImgsPathArray[i] = drillsImgsPathArray[i];
   }
 });
 
@@ -29,6 +41,7 @@ hammersTag.addEventListener("click", (e)=>{
   {
    drillsImgs[i].setAttribute('src', hammersImgsPathArray[i]);
    drillsImgs[i].setAttribute('alt', "un marteau");
+   defaultToolsImgsPathArray[i] = hammersImgsPathArray[i];
   }
 });
 
@@ -38,6 +51,7 @@ screwDriverTag.addEventListener("click", (e)=>{
   {
    drillsImgs[i].setAttribute('src', screwDrvImgsPathArray[i]);
    drillsImgs[i].setAttribute('alt', "une tourne vis");
+   defaultToolsImgsPathArray[i] = screwDrvImgsPathArray[i];
   }
 });
 /* ==== Miscellaneous tag event lstner ===*/
@@ -46,6 +60,7 @@ miscllnTag.addEventListener("click", (e)=>{
   {
    drillsImgs[i].setAttribute('src', miscllnImgsPathArray[i]);
    drillsImgs[i].setAttribute('alt', "outils divers");
+   defaultToolsImgsPathArray[i] = miscllnImgsPathArray[i];
   }
 });
 
@@ -58,4 +73,206 @@ for(i = 0; i < drillsWrapper.children.length; i++)
     drillsWrapper.children[i].style.display = 'none';
   }
 }
-//setTimeout(()=>{wrapper.scroll(-100, 0)}, 2000);
+//slides controls
+function isDoorContainerLeftScrollable()
+{
+  if(doorsImgs[0].getAttribute('src') == doorsImgsArray[0])
+  {
+    leftDoor.style.display = "none";
+  }
+  else
+  {
+     leftDoor.style.display = "flex";
+  }
+}
+
+function isDoorContainerRightScrollable()
+{
+  if(doorsImgs[1].getAttribute('src') == doorsImgsArray[doorsImgsArray.length-1])
+  {
+    rightDoor.style.display = "none";
+  }
+  else
+  {
+     rightDoor.style.display = "flex";
+  }
+}
+isDoorContainerLeftScrollable();
+let doorIndex1 = 0;
+let doorIndex2 = 1;
+leftDoor.addEventListener("click", (e) => {
+    //some code
+    doorIndex1 -= 2;
+    doorIndex2 -= 2;
+    if(doorIndex1 <= 0)
+    {
+      leftDoor.style.display = "none";
+      rightDoor.style.display = "flex";
+      //doorIndex1 = doorsImgs.length-1;
+      //doorIndex2 = doorsImgs.length;
+    }
+    
+    doorsImgs[0].setAttribute("src",doorsImgsArray[doorIndex1]);
+    doorsImgs[1].setAttribute("src",doorsImgsArray[doorIndex2]);
+    
+    //isDoorContainerLeftScrollable();
+    isDoorContainerRightScrollable();
+
+});
+
+rightDoor.addEventListener("click", (e) => {
+    doorIndex1 += 2;
+    doorIndex2 += 2;
+    if(doorIndex2 >= doorsImgs.length-2)
+    {
+      rightDoor.style.display = "none";
+    }
+    else
+    {
+      rightDoor.style.display = "flex";
+    }
+    
+    doorsImgs[0].setAttribute("src",doorsImgsArray[doorIndex1]);
+    doorsImgs[1].setAttribute("src",doorsImgsArray[doorIndex2]);
+    
+    isDoorContainerLeftScrollable();
+    let evValue = "Right click on door";
+    printEv();
+});
+
+// Window control buttons
+function isWindowContainerLeftScrollable()
+{
+  if(windowImgs[0].getAttribute('src') == windowImgsArray[0])
+  {
+    leftWindow.style.display = "none";
+  }
+  else
+  {
+    leftWindow.style.display = "flex";
+  }
+}
+function isWindowContainerrightScrollable()
+{
+  if(windowImgs[1].getAttribute('src') == windowImgsArray[windowImgsArray.length-1])
+  {
+    rightWindow.style.display = "none";
+  }
+  else
+  {
+    rightWindow.style.display = "flex";
+  }
+}
+isWindowContainerLeftScrollable();
+let windowIndex1 = 0;
+let windowIndex2 = 1;
+leftWindow.addEventListener("click", (e) => {
+  
+    windowIndex1 -= 2;
+    windowIndex2 -= 2;
+    if(windowIndex1 <= 0)
+    {
+      leftWindow.style.display = "none";
+      rightWindow.style.display = "flex";
+    }
+    else
+    {
+      leftWindow.style.display = "flex";
+    }
+    
+    windowImgs[0].setAttribute("src",windowImgsArray[windowIndex1]);
+    windowImgs[1].setAttribute("src",windowImgsArray[windowIndex2]);
+    
+    isWindowContainerLeftScrollable();
+    isWindowContainerrightScrollable();
+});
+
+rightWindow.addEventListener("click", (e) => {
+  
+    windowIndex1 += 2;
+    windowIndex2 += 2;
+    if(windowIndex2 >= windowImgs.length-1)
+    {
+      rightWindow.style.display = "none";
+    }
+    else
+    {
+      rightWindow.style.display = "flex";
+    }
+    
+    windowImgs[0].setAttribute("src",windowImgsArray[windowIndex1]);
+    windowImgs[1].setAttribute("src",windowImgsArray[windowIndex2]);
+    
+    isWindowContainerLeftScrollable();
+   //isWindowContainerrightScrollable();
+});
+
+
+// TOOLS CONTROLS
+/*
+1- By default the defaultToolsImgsPathArray will contain the drills img [yes]
+*/
+/*
+2- but when we click on given tag the imgs in that array will be dynamically replaced by other imgs related to the clicked tag. [yes]
+*/
+console.log(defaultToolsImgsPathArray);
+console.log(drillsImgs);
+
+/*
+// I'll use a for Loop to iterate on the arrays and replace each items in them.
+
+3- The tools arrows will have a class leftTools-rightTools[yes]
+*/
+function isToolsContainerLeftScrollable()
+{
+  if(drillsImgs[0].getAttribute('src') == defaultToolsImgsPathArray[0])
+    {
+      leftTool.style.display = "none";
+    }
+    else
+    {
+      leftTool.style.display = "flex";
+    }
+}
+function isToolsContainerRightScrollable()
+{
+  if(drillsImgs[1].getAttribute('src') == defaultToolsImgsPathArray[defaultToolsImgsPathArray.length-1])
+  {
+    rightTool.style.display = "none";
+  }
+  else
+  {
+    rightTool.style.display = "flex";
+  }
+}
+isToolsContainerLeftScrollable();
+
+let toolIndex1 = 0;
+let toolIndex2 = 1;
+
+leftTool.addEventListener("click", (e) => {
+   toolIndex1 -= 2;
+   toolIndex2 -= 2;
+   
+   drillsImgs[0].setAttribute('src', defaultToolsImgsPathArray[toolIndex1]);
+   drillsImgs[1].setAttribute('src', defaultToolsImgsPathArray[toolIndex2]);
+    isToolsContainerLeftScrollable();
+    isToolsContainerRightScrollable();
+});
+rightTool.addEventListener("click", (e) => {
+   toolIndex1 += 2;
+   toolIndex2 += 2;
+   if(toolIndex2 >= defaultToolsImgsPathArray.length-2)
+   {
+     rightTool.style.display = "none";
+   }
+   else
+   {
+     rightTool.style.display = "flex";
+   }
+   
+   drillsImgs[0].setAttribute('src', defaultToolsImgsPathArray[toolIndex1]);
+   drillsImgs[1].setAttribute('src', defaultToolsImgsPathArray[toolIndex2]);
+    isToolsContainerLeftScrollable();
+    //isToolsContainerRightScrollable();
+});
--- elements.js
+++ elements.js
@@ -1,11 +1,22 @@
 export {
   slides,
+  leftDoor,
+  rightDoor,
+  leftWindow,
+  rightWindow,
+  leftTool,
+  rightTool,
   drillsWrapper,
+  doorsImgs,
+  windowImgs,
   drillsImgs,
   drillsTag,
   hammersTag,
   screwDriverTag,
   miscllnTag,
+  defaultToolsImgsPathArray,
+  doorsImgsArray,
+  windowImgsArray,
   drillsImgsPathArray,
   hammersImgsPathArray,
   screwDrvImgsPathArray,
@@ -13,16 +24,61 @@ export {
 };
 
 /*selecting the needed elements on the document*/
-
+const leftDoor = document.querySelector('.leftDoor');
+const rightDoor = document.querySelector('.rightDoor');
+const leftWindow = document.querySelector(".leftWindow");
+const rightWindow = document.querySelector(".rightWindow");
+const leftTool = document.querySelector(".leftTools");
+const rightTool = document.querySelector(".rightTools");
 const slides = document.querySelectorAll('.img_container');
 const drillsWrapper= document.querySelector(".drills_wrapper");
+
+const doorsImgs = document.querySelectorAll(".doorsImgs");
+const windowImgs = document.querySelectorAll(".windowImgs");
 const drillsImgs = document.querySelectorAll('.drills');
+
 const drillsTag = document.getElementById('drills');
 const hammersTag = document.getElementById('hammers');
 const screwDriverTag = document.getElementById('screw_Driver');
 const miscllnTag = document.getElementById('miscellanious');
 
 //storing the images' pathes in some Arrays
+const doorsImgsArray = [
+  "Images/Portes/depositphotos_4089844-stock-photo-closed-single-red-door.jpg",
+  "Images/Portes/depositphotos_105613604-stock-photo-shut-white-wooden-door-on~2.jpg",
+  "Images/Portes/depositphotos_109967374-stock-photo-collection-of-isolated-black-doors.jpg",
+  "Images/Portes/depositphotos_13814713-stock-photo-dark-brown-wooden-door.jpg",
+  "Images/Portes/depositphotos_15418939-stock-photo-new-design.jpg",
+  "Images/Portes/depositphotos_4911018-stock-photo-white-empty-room-open-wooden.jpg",
+  "Images/Portes/depositphotos_5483973-stock-photo-wooden-door-over-white-background.jpg",
+  "Images/Portes/depositphotos_56492521-stock-photo-new-steel-three-bolt-door.jpg",
+  "Images/Portes/depositphotos_8283559-stock-photo-doors-with-welcome-mat.jpg",
+  "Images/Portes/TRIANON-Porte-intérieure-bois-Style-Pasquet-Menuiseries-ayous.jpg",
+  "Images/Portes/porte-s-ouvre-nouvelle-composition-du-monde_23-2150172143~2.jpg",
+  "Images/Portes/porte-bois-brown_1194-635.webp"
+  ];
+const windowImgsArray = [
+    "Images/Window/ensemble-fenetres-cintrees-realistes_1284-11041.webp",
+    "Images/Window/double-fenetre-volet-roulant-haut-bas_107791-2786.webp",
+    "Images/Window/fenetres-maisons-style-europeen_1253-1497.webp",
+    "Images/Window/fenetre-ouverte-vintage-classique-bois_88088-586.webp",
+    "Images/Window/ensemble-portes-entree-modernes-noir-blanc_88088-746.webp",
+    "Images/Window/fenetre-dans-cadre-classique-fronton-garniture-element-decoration-architecturale-du-batiment_87424-440.webp",
+    "Images/Window/fenetre-large-moderne-plastique-blanc-noir_88088-584.webp",
+    "Images/Window/fenetre-bois-rose-rouge-rebord-fenetre-vecteur_259139-1887.webp"
+  ];
+const defaultToolsImgsPathArray = [
+  "Images/Drills/perceuse-sans-fil-isolee_87540-202.webp",
+  "Images/Drills/perceuse-sans-fil_770606-3840.webp",
+  "Images/Drills/tournevis-sans-fil-isole-fond-blanc-instrument-accumulateur_508659-5701.webp",
+  "Images/Drills/perforateur-buses-table-blanche_23-2148180533.webp",
+  "Images/Drills/machine-forage-ensemble-icones-realistes-images-isolees-perceuses-vis-detachables-illustration-vectorielle-appareil-forage_1284-72292.webp",
+  "Images/Drills/perceuse-electrique-orange-cable-isole-blanc-trace-detourage_101296-851.webp",
+  "Images/Drills/perceuse-visseuse-sans-fil-electrique-isole-fond-blanc-outil-reparation-domicile-professionnel_105428-4473.webp",
+  "Images/Drills/bouteille-sauce-piquante_924945-1430.webp",
+  "Images/Drills/perceuse-pratique-ensemble-differents-forets-lunettes-securite-illustration-vectorielle-composition-realiste_1284-73112.webp",
+  "null/leadsNoWhere"
+  ];
 const drillsImgsPathArray = [
   "Images/Drills/perceuse-electrique-orange-cable-isole-blanc-trace-detourage_101296-851.webp",
   "Images/Drills/perceuse-sans-fil-isolee_87540-202.webp",
